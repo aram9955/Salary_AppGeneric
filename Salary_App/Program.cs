047822785776e44f36dd81e9e05e418d54dc64e9
Aram Baghdasarian <aram99551@gmail.com>
2021-10-06 18:17:25 +0400

    Add project files.

---
+++ Salary_App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+namespace Salary_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salary<string> salary = new Salary<string>();
+            var result = salary.GetEnumerator();
+
+            for (int i = 0; result.MoveNext(); i++)
+            {
+                Console.WriteLine(result.Current);
+            }
+        }
+    }
+}
---
+++ Salary_App/Salary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Salary_App
+{
+    public class Salary<T> : IEnumerable
+    {
+        protected int[] salary = new int[] { 200000, 60000, 500000, 400000 };
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in salary)
+            {
+                if (item < 500000)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
